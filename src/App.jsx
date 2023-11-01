f5c5ae6fc73b6f10e716efadaa0a4608cd4aa009
Nooruz <rrisovanie638@gmail.com>
2023-11-01 16:20:27 +0300

    delete in App Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { Button } from "./button/Button"
-
 export function App() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <Button isDone="green">Позвонить нам</Button>
     </div>
   )
 }
